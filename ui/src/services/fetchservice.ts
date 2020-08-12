80b38fd03dc7cf6315db2db23575306ccf600ff9
sayan-mitra <sayan.besu@gmail.com>
2020-08-13 00:17:34 +0530

    changes

--- ui/src/services/fetchservice.ts
+++ ui/src/services/fetchservice.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const BACKEND_BASE_URL =
-  process.env.QUERY_GEN_BACKEND_URL || 'http://localhost:3001'
+  process.env.REACT_APP_QUERY_GEN_BACKEND_URL || 'http://localhost:3001'
 
 const fetchService = () => {
     return Axios.create({
